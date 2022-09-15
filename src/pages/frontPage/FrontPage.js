5ca28136adef52bcc064aba21bf00fc6204ec555
Sanay1910 <sanay1910@gmail.com>
2022-09-15 02:57:28 +0200

    Added Bootstrap to the project

--- src/pages/frontPage/FrontPage.js
+++ src/pages/frontPage/FrontPage.js
@@ -1,24 +1,21 @@
 import React, { useRef, useState } from "react";
 import "./FrontPage.css"
 
-var name = "Joe";
-//const promptValue = prompt("Type in your new name!");
 function FrontPage() {
-    const [name, setName] = useState('randomer'); 
+    const [name, setName] = useState(''); 
     const onChangeHandler = event => setName(event.target.value);
     const handleClick = event => {
         event.preventDefault();
     }
     console.log(name);
-    const node = useRef(null);
     return(
-        <div>
-            <h1 class="cname">Welcome, {name}</h1>
+        <div className="top-modal">
+            <h1 className="welcome">Welcome, {name}</h1>
             <h2>This is your dashboard</h2>
             <br/>
             <p>Do you wish to change your name?</p>
-            <input type="text" id="inputName" name="inputName" onChange={onChangeHandler} value={name}></input>
-            <button onClick={handleClick}>Change</button>
+            <input type="text" id="inputName" name="inputName" onChange={onChangeHandler} value={name} placeholder="Enter your name here"></input>
+            <button type="button" className="btn btn-primary" onClick={handleClick}>Change</button>
         </div>
     )
     
