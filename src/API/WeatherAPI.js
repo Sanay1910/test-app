b96b1ebe8bd1ba24aa2e13c969a28718406b8875
Sanay1910 <sanay1910@gmail.com>
2022-09-16 13:44:12 +0200

    WIP weatherforecast include time and degrees

--- src/API/WeatherAPI.js
+++ src/API/WeatherAPI.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './WeatherAPI.css';
 
 class WeatherAPI extends Component {
 
@@ -19,7 +20,8 @@ class WeatherAPI extends Component {
         .then((response) => {
             this.setState({
                 weatherForecast: response.data,
-                time: response.data.hourly.time
+                time: response.data.hourly.time,
+                temp: response.data.hourly.temperature_2m
             });
         })
     }
@@ -27,14 +29,20 @@ class WeatherAPI extends Component {
     render() {
         const {weatherForecast} = this.state
         const {time} = this.state
-        
-        console.log(time)
+        const {temp} = this.state
         return (
             <div>
-              <h1>Weather forecast in Oslo</h1>
+              <h1>Weather forecast today in Oslo</h1>
               <h2>Timezone: {weatherForecast.timezone}</h2>
-              <h3>Weather Forecast</h3>
-              {time.map(tim => <li>{tim}</li>)}
+              <h3>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-brightness-low" viewBox="0 0 16 16">
+                    <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm.5-9.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 11a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm5-5a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm-11 0a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm9.743-4.036a.5.5 0 1 1-.707-.707.5.5 0 0 1 .707.707zm-7.779 7.779a.5.5 0 1 1-.707-.707.5.5 0 0 1 .707.707zm7.072 0a.5.5 0 1 1 .707-.707.5.5 0 0 1-.707.707zM3.757 4.464a.5.5 0 1 1 .707-.707.5.5 0 0 1-.707.707z"></path>
+                </svg>
+                Weather Forecast</h3>
+              <div>
+              <div className='time-table'>{time.map(tim => <ul>{tim}</ul>)}</div>
+              <div className='temp-table'>{temp.map(tem => <ul>{tem} Celcius</ul>)}</div>
+              </div>
             </div>
         )
     }
